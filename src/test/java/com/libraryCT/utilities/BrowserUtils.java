9814d4b4110893896c0de7d8f82dd0ab0658d3df
Abdujilil <abdujilil0127@hotmail.com>
2021-02-19 14:17:51 -0500

    save before merge

--- src/test/java/com/libraryCT/utilities/BrowserUtils.java
+++ src/test/java/com/libraryCT/utilities/BrowserUtils.java
@@ -1,9 +1,18 @@
 package com.libraryCT.utilities;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BrowserUtils {
     public static void sleep(double second){
         try { Thread.sleep((long)(1000*second)); }
         catch (InterruptedException e) { System.out.println("Something happende in sleep method"); }
+    }
+
+    public static WebElement waitUntilClickable(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 5);
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
 }
